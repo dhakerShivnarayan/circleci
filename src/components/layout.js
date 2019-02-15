cc64bfc3a84755424cf6f3e0aee2594f009ea5e3
Shiv <shivnarayan.dhaker@raweng.com>
2019-02-15 17:26:47 +0530

    added workflow

--- src/components/layout.js
+++ src/components/layout.js
@@ -29,16 +29,12 @@ const Layout = ({ children }) => (
           <main>{children}</main>
           <footer>
             © {new Date().getFullYear()}, Built with
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
+            <a href="https://www.gatsbyjs.org"> Gatsby :)</a>
           </footer>
         </div>
       </>
     )}
   />
 )
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
 
 export default Layout
